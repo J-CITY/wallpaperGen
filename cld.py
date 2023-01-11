e55393de159773ff7e2844d0cf4227971d323636
Daniil Glushchenko <daniil.glushchenko@mytona.com>
2023-01-11 20:58:44 +0600

    Add colorize image

---
+++ cld.py
@@ -0,0 +1,71 @@
+import cv2
+import sys
+import numpy as np
+
+image_filename = sys.argv[1]
+color_image = cv2.imread(image_filename, cv2.IMREAD_COLOR)
+
+height = color_image.shape[0]
+width  = color_image.shape[1]
+channel = color_image.shape[2]
+
+gray_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
+
+
+M_0 = np.average(gray_image)
+N = 32
+threshold = 0.2
+
+local_CLD = np.zeros((height, width, N))
+for i in range(N):
+    angle = i * 2 * np.pi / N
+    for y in range(height):
+        for x in range(width):
+            progress = np.round(100*(i*height*width+y*width+x)/(N*width*height), 2)
+            print( "Progress 1: ", str(progress)+"%", end='\r' )
+            l = 0
+            s = gray_image[y, x].astype(float)
+            while(True):
+                l += 1
+                xx = x + int(l * np.cos(angle))
+                yy = y + int(l * np.sin(angle))
+                if not (yy < 0 or yy >= height or xx <= 0  or xx >= width):
+                    s += gray_image[yy, xx]
+                if (s/l - M_0) / M_0 <= threshold:
+                    local_CLD[y,x,i] = l
+                    break
+L = 4
+brushstroke_size = np.zeros((height, width))
+for y in range(height):
+    for x in range(width):
+        brushstroke_size[y,x] = N * L / np.sum(local_CLD[y,x])
+
+oil_image = np.zeros((height, width, channel))
+for y in range(height):
+    for x in range(width):
+        progress = np.round(100*(y*width+x)/(width*height), 2)
+        print( "Progress 2: ", str(progress)+"%", end='\r' )
+        R = int(np.ceil(brushstroke_size[y,x]))
+        local_histogram = np.zeros(256)
+        local_channel_count = np.zeros((channel, 256))
+        for dy in range(-R, R):
+            for dx in range(-R, R):
+                yy = y+dy
+                xx = x+dx
+                if dy*dy + dx*dx > R*R:
+                    continue
+                if yy < 0  or yy >= height or xx <= 0  or xx >= width:
+                    continue
+                intensity = gray_image[yy, xx]
+                local_histogram[intensity] += 1
+                for c in range(channel):
+                    local_channel_count[c, intensity] += color_image[yy, xx, c]
+
+        max_intensity = np.argmax(local_histogram)
+        max_intensity_count = local_histogram[max_intensity]
+        for c in range(channel):
+            oil_image[y,x,c] = local_channel_count[c, max_intensity] / max_intensity_count
+
+oil_image = oil_image.astype('int')
+cv2.imwrite("result.jpg", oil_image)
+cv2.imwrite("result1.jpg", oil_image[50:100, 50:150])
---
+++ colorize.py
@@ -0,0 +1,254 @@
+from enum import Enum
+import argparse
+from PIL import Image
+import json
+import cv2
+
+class Ditherer(Enum):
+    none = 0,
+    floyd_steinberg = 1,
+    atkinson = 2,
+    jjn = 3,
+    burkes = 4,
+    sierra = 5,
+    sierra_lite = 6
+
+    def __str__(self):
+        return self.value
+
+class Config:
+    inFile = "in.jpg"
+    outFile = "out.png"
+    theme = "Aura"
+    dither = Ditherer.none
+
+config = Config()
+channels = 0
+height = 0
+width = 0
+pixels = None
+
+def parseArgs():
+    parser = argparse.ArgumentParser(prog='colorizer')
+    parser.add_argument('--inFile', type=str, help='Input image file')
+    parser.add_argument('--outFile', type=str, help='Output image file name')
+    parser.add_argument('--theme', type=str, help='Palette theme')
+    parser.add_argument('--dither', choices=Ditherer.__members__, help='Ditherer type')
+    args = parser.parse_args()
+
+    if args.inFile != None:
+        config.inFile = args.inFile
+    if args.outFile != None:
+        config.outFile = args.outFile
+    if args.dither != None:
+        config.dither = args.dither
+    if args.theme != None:
+        config.theme = args.theme
+
+def updatePixel(img, x, y, err, mul, div):
+    if x < 0 or x >= width:
+        return
+    if y < 0 or x >= height:
+        return
+    #pixel = img[y, x]
+    #r = pixel[0] + err[0] * mul / div
+    #g = pixel[1] + err[1] * mul / div
+    #b = pixel[2] + err[2] * mul / div
+
+    r = img.getpixel((x, y))[0] + err[0] * mul / div
+    g = img.getpixel((x, y))[1] + err[1] * mul / div
+    b = img.getpixel((x, y))[2] + err[2] * mul / div
+
+    #r = pixels[x, y][0] + err[0] * mul / div
+    #g = pixels[x, y][1] + err[1] * mul / div
+    #b = pixels[x, y][2] + err[2] * mul / div
+
+    clamp = lambda n, minn, maxn: max(min(maxn, n), minn)
+
+    clamp(r, 0, 255)
+    clamp(g, 0, 255)
+    clamp(b, 0, 255)
+
+    mg[y, x] = (r,g,b)
+    img.puypixel((x,y), (r,g,b))
+    #pixels[x, y] = (r,g,b)
+
+
+def dither_floyd_steinberg(img, x, y, err):
+    updatePixel(img, x + 1, y + 0, err, 7, 16)
+    if y + 1 >= height:
+        return
+    updatePixel(img, x - 1, y + 1, err, 3, 16)
+    updatePixel(img, x + 0, y + 1, err, 5, 16)
+    updatePixel(img, x + 1, y + 1, err, 1, 16)
+
+def dither_atkinson(img, x, y, err):
+    updatePixel(img, x + 1, y + 0, err, 1, 8)
+    updatePixel(img, x + 2, y + 0, err, 1, 8)
+    if y + 1 >= height:
+        return
+    updatePixel(img, x - 1, y + 1, err, 1, 8)
+    updatePixel(img, x + 0, y + 1, err, 1, 8)
+    updatePixel(img, x + 1, y + 1, err, 1, 8)
+    if y + 2 >= height:
+        return
+    updatePixel(img, x + 1, y + 2, err, 1, 8)
+
+def dither_jjn(img, x, y, err):
+    updatePixel(img, x + 1, y + 0, err, 7, 48)
+    updatePixel(img, x + 2, y + 0, err, 5, 48)
+    if y + 1 >= height:
+        return
+    updatePixel(img, x - 2, y + 1, err, 3, 48)
+    updatePixel(img, x - 1, y + 1, err, 5, 48)
+    updatePixel(img, x + 0, y + 1, err, 7, 48)
+    updatePixel(img, x + 1, y + 1, err, 5, 48)
+    updatePixel(img, x + 2, y + 1, err, 3, 48)
+    if y + 2 >= height:
+        return
+    updatePixel(img, x - 2, y + 2, err, 1, 48)
+    updatePixel(img, x - 1, y + 2, err, 3, 48)
+    updatePixel(img, x + 0, y + 2, err, 5, 48)
+    updatePixel(img, x + 1, y + 2, err, 3, 48)
+    updatePixel(img, x + 2, y + 2, err, 1, 48)
+
+def dither_burkes(img, x, y, err):
+    updatePixel(img, x + 1, y + 0, err, 8, 32)
+    updatePixel(img, x + 2, y + 0, err, 4, 32)
+    if y + 1 >= height:
+        return
+    updatePixel(img, x - 2, y + 1, err, 2, 32)
+    updatePixel(img, x - 1, y + 1, err, 4, 32)
+    updatePixel(img, x + 0, y + 1, err, 8, 32)
+    updatePixel(img, x + 1, y + 1, err, 4, 32)
+    updatePixel(img, x + 2, y + 1, err, 2, 32)
+
+def dither_sierra(img, x, y, err):
+    updatePixel(img, x + 1, y + 0, err, 5, 32)
+    updatePixel(img, x + 2, y + 0, err, 3, 32)
+    if y + 1 >= height:
+        return
+    updatePixel(img, x - 2, y + 1, err, 2, 32)
+    updatePixel(img, x - 1, y + 1, err, 4, 32)
+    updatePixel(img, x + 0, y + 1, err, 5, 32)
+    updatePixel(img, x + 1, y + 1, err, 4, 32)
+    updatePixel(img, x + 2, y + 1, err, 2, 32)
+    if y + 2 >= height:
+        return
+    updatePixel(img, x - 1, y + 2, err, 2, 32)
+    updatePixel(img, x + 0, y + 2, err, 3, 32)
+    updatePixel(img, x + 1, y + 2, err, 2, 32)
+
+def dither_sierra_lite(img, x, y, err):
+    updatePixel(img, x + 1, y + 0, err, 2, 4)
+    if y + 1 >= height:
+        return
+    updatePixel(img, x - 1, y + 1, err, 1, 4)
+    updatePixel(img, x + 0, y + 1, err, 1, 4)
+
+from multiprocessing.pool import ThreadPool as Pool
+# from multiprocessing import Pool
+
+pool_size = 15
+
+def worker(y, img, palette, dither):
+    global config
+    global width
+    global height
+    global channels
+    for x in range(0, width):
+        old_color = img.getpixel((x, y))
+        #old_color = img[y, x]
+        error = None #error color
+        min_diff = -1
+        for color in palette:
+            dr = old_color[0] - color[0]
+            dg = old_color[1] - color[1]
+            db = old_color[2] - color[2]
+            diff = dr * dr + dg * dg + db * db
+            if min_diff == -1 or diff < min_diff:
+              min_diff = diff
+              #img[y,x] = color
+              img.putpixel((x,y), color)
+              error = (dr, dg, db)
+        if dither == Ditherer.none:
+            break
+        if dither == Ditherer.floyd_steinberg:
+            dither_floyd_steinberg(img, x, y, error)
+        if dither == Ditherer.atkinson:
+            dither_atkinson(img, x, y, error)
+        if dither == Ditherer.jjn:
+            dither_jjn(img, x, y, error)
+        if dither == Ditherer.burkes:
+            dither_burkes(img, x, y, error)
+        if dither == Ditherer.sierra:
+            dither_sierra(img, x, y, error)
+        if dither == Ditherer.sierra_lite:
+            dither_sierra_lite(img, x, y, error)
+def recolor(img, palette, dither):
+    global config
+    global width
+    global height
+    global channels
+    
+    pool = Pool(pool_size)
+    for y in range(0, height):
+        #print(y)
+        #worker(y, img, palette, dither)
+        pool.apply_async(worker, args=(y, img, palette, dither))
+    pool.close()
+    pool.join()
+
+def main():
+    global config
+    global width
+    global height
+    global channels
+    global pixels
+
+    parseArgs()
+    print(config.inFile)
+    print(config.outFile)
+    print(config.dither)
+    print(config.theme)
+
+    if config.inFile == "":
+        print("Input file is empty")
+        return
+
+    #print(config.inFile)
+    
+    #image = cv2.imread(config.inFile)
+    #width = image.shape[1]
+    #height = image.shape[0]
+    #channels = image.shape[2]
+
+    image = Image.open(config.inFile)
+    channels = len(image.split())
+    width, height = image.size
+    pixels = image.load()
+
+    data = json.load(open('themes.json'))
+    themeData = data["themes"][0]
+    for value in data["themes"]:
+        #print(value["name"])
+        if config.theme == value["name"]:
+            themeData = value
+            break
+
+    palette = []
+    for k, v in value.items():
+        if k == "name":
+            continue
+        h = v.lstrip('#')
+        palette.append(tuple(int(h[i:i+2], 16) for i in (0, 2, 4)))
+    #print(palette)
+    recolor(image, palette, config.dither)
+
+    print("Save to: " + config.outFile)
+    image.save(config.outFile)
+    #cv2.imwrite(config.outFile, image)
+
+
+if __name__ == "__main__":
+    main()
